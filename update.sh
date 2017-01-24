b20f4062b658e12d0ed4aa5d6b5f33c4d4dc7ae4
make-ing <bckr.florian@gmail.com>
2017-01-24 13:19:16 +0100

    initial commit of update.sh script

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+printf "Downloading new version...\n"
+git fetch --all
+git reset --hard origin/master
+printf "Remove old version...\n"
+sudo -u pi /home/pi/oprint/bin/pip uninstall -y Octoprint-netconnectd
+printf "Installing new version...\n"
+sudo -u pi /home/pi/oprint/bin/python setup.py install
