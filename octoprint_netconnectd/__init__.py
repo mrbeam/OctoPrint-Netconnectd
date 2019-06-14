2d3371ac28044e6d20000e7d89f74d30b49d34e3
Iratxe Espartza Chueca <iratxe@mr-beam.org>
2019-06-14 18:23:42 +0200

    Add Spanish translation

--- octoprint_netconnectd/__init__.py
+++ octoprint_netconnectd/__init__.py
@@ -10,6 +10,7 @@ import logging
 import threading
 import netifaces
 from flask import jsonify, make_response
+from flask.ext.babel import gettext
 
 import octoprint.plugin
 from octoprint.server import admin_permission
@@ -59,7 +60,7 @@ class NetconnectdSettingsPlugin(octoprint.plugin.SettingsPlugin,
 
 	def get_template_configs(self):
 		return [
-			dict(type="settings", name="Network Connection")
+			dict(type="settings", name=gettext("Network Connection"))
 		]
 
 	##~~ SimpleApiPlugin API
